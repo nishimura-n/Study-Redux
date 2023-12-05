4e94260af0890f287264d3b0dc30400592019fe6
Nishimura <Nishimura@NIshimura.local>
2023-12-05 23:46:27 +0900

    買い物かごの中身を出力するコンテナを作成ver1

--- redux-shopping-app/src/App.js
+++ redux-shopping-app/src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import CartCotainer from "./components/CartCotainer";
 
 function App() {
   return (
     <main>
       <Navbar />
+      <CartCotainer />
     </main>
   );
 }
