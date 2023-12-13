6a12e62ff9bfeb8a22a189feb61a6e84fa2af9ca
Nishimura <Nishimura@NIshimura.local>
2023-12-13 23:46:37 +0900

    storeの中身から商品情報を取り出して出力

--- redux-shopping-app/src/components/CartCotainer.js
+++ redux-shopping-app/src/components/CartCotainer.js
@@ -21,7 +21,7 @@ const CartCotainer = () => {
       </header>
       <div>
         {cartItems.map((item) => {
-          return <CartItem key={item.id} />;
+          return <CartItem key={item.id} {...item} />;
         })}
       </div>
       <footer>
